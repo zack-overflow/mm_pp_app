b83f464f80001ad026dca49b7d13bffa61812bf8
Zack Gottesman <zachary.s.gottesman.22@dartmouth.edu>
2025-03-20 18:59:21 -0400

    add number of players remaining count

--- create_scoreboard.py
+++ create_scoreboard.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import json
-from constants import JSON_FILE_PATH
+from constants import TEAMS_ALIVE_MASK
 from get_entrant_data import get_entrant_data
 
 def get_multiplier(seed):
@@ -39,5 +39,16 @@ def create_scoreboard(pikap):
                 
         combined_data[entrant]['score'] = total_points
         combined_data[entrant]['sum_multiplier'] = sum_multiplier
+
+        # Sum the number of players alive for each entrant
+        alive_count = 0
+        for player, data in player_data.items():
+            if data['team'] in TEAMS_ALIVE_MASK:
+                if TEAMS_ALIVE_MASK[data['team']] == 1:
+                    alive_count += 1
+            else:
+                print(f"Warning: {data['team']} not found in TEAMS_ALIVE_MASK")
+
+        combined_data[entrant]['alive_count'] = alive_count
 
     return combined_data
